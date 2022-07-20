d502de2007e5f885d0265ab4514cbaa3cede1719
ArgoWack <45313830+ArgoWack@users.noreply.github.com>
2022-07-20 23:43:28 +0200

    Added comments

--- TGL_Practice_1/Program.cs
+++ TGL_Practice_1/Program.cs
@@ -3,10 +3,20 @@ using static System.Console;
 using System.Threading;
 namespace TGL_Practice_1
 {
+    /*
+            Short Summary:
+
+    Program symulates work of Timber company which uses 2 kinds of workers "TimberMan"'s and "Experienced TimberMan"'s and hires 5 workers of each kind.
+    Their job is to gather logs. Each workers is separate thread. To avoid sync problems I use lock when storing logs. 
+    The work is split into 4 shifts each separated with 5000ms break. The amount of logs gatherd by each worker is based on generated seed and worker grade.
+    I also added numbers of Threads info. They aren't essential for program but I thought that it would show better how is it working. 
+
+    */
     class Program
     {
         class Timber_company
         {
+            //here logs of all workers are stored 
             public static int log;
 
             //lock for returning wood
@@ -25,11 +35,12 @@ namespace TGL_Practice_1
                 static int threadSpecificData;
                 public override void cut_timber()
                 {
+                    //returns number of thread
                     threadSpecificData = Thread.CurrentThread.ManagedThreadId - 4;
                     lock (log_deposition_lock)
                     {
                         Random seed = new Random();
-
+                        //generates random number with [0,9]
                         int chances = seed.Next(0, 10);
                         int gatherd_logs = 0;
 
@@ -70,11 +81,12 @@ namespace TGL_Practice_1
                 static int threadSpecificData;
                 public override void cut_timber()
                 {
+                    //returns number of thread
                     threadSpecificData = Thread.CurrentThread.ManagedThreadId - 4;
                     lock (log_deposition_lock)
                     {
                         Random seed = new Random();
-
+                        //generates random number with [0,9]
                         int chances = seed.Next(0, 10);
                         int gatherd_logs=0;
 
@@ -118,15 +130,19 @@ namespace TGL_Practice_1
                 Timber_company.Experienced_TimberMan start_Experienced_TimberMans = new Timber_company.Experienced_TimberMan();
 
                 int work_shifts = 1;
+                // iteration thro work_shifts. Set on 4 but could be any number. 
                 while (work_shifts < 5)
                 {
                     WriteLine("Start of " + work_shifts + " work shift");
+
+                    //5 timberMan's threads
                     Thread TimberMan_1 = new Thread(start_timberMans.cut_timber);
                     Thread TimberMan_2 = new Thread(start_timberMans.cut_timber);
                     Thread TimberMan_3 = new Thread(start_timberMans.cut_timber);
                     Thread TimberMan_4 = new Thread(start_timberMans.cut_timber);
                     Thread TimberMan_5 = new Thread(start_timberMans.cut_timber);
 
+                    //5 Experienced timberMan's threads
                     Thread Experienced_TimberMan_1 = new Thread(start_Experienced_TimberMans.cut_timber);
                     Thread Experienced_TimberMan_2 = new Thread(start_Experienced_TimberMans.cut_timber);
                     Thread Experienced_TimberMan_3 = new Thread(start_Experienced_TimberMans.cut_timber);
